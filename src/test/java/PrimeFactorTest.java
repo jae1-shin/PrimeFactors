46ef01381286c8c2f062380a0b0a32a95fb2182d
jae1-shin <blessed9589@gmail.com>
2025-06-04 10:08:27 +0900

    first commit

---
+++ src/test/java/PrimeFactorTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeFactorTest {
+
+    @Test void test() {
+        assertEquals(10, 10);
+    }
+
+
+}
